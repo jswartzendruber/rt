456d09922fee171a5596538ae9e209ac82918f65
Jason <jdswart2@svsu.edu>
2022-05-03 20:33:49 -0400

    add lines

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,34 @@ use std::{env, fs, path::PathBuf};
 
 fn main() {
     let current_dir = env::current_dir().expect("Could not get current directory.");
-    recursive_walk(current_dir, 0)
+    recursive_walk(current_dir, "", 0)
 }
 
-fn recursive_walk(current_dir: PathBuf, depth: u32) {
-    let mut paths: Vec<_> = fs::read_dir(current_dir)
-        .unwrap()
-        .map(|r| r.unwrap())
+fn recursive_walk(dir: PathBuf, prefix: &str, mut depth: u32) {
+    // display root
+    if depth == 0 {
+        depth += 1;
+        println!(".");
+    }
+
+    let mut paths: Vec<_> = fs::read_dir(dir)
+        .expect("Invalid ReadDir")
+        .map(|r| r.expect("Invalid DirEntry"))
         .filter(|d| !d.file_name().to_string_lossy().starts_with(".")) // hide dotfiles
         .collect();
 
     paths.sort_by_key(|dir| dir.path());
 
-    for entry in paths {
-        let mut spacing = String::with_capacity(depth as usize);
-        for _ in 1..depth {
-            spacing += "  "
-        }
-
-        println!("{}{}", spacing, entry.file_name().to_string_lossy());
-        if let Ok(file_type) = entry.file_type() {
-            if file_type.is_dir() {
-                recursive_walk(entry.path(), depth + 2)
+    for (idx, entry) in paths.iter().enumerate() {
+        if idx == paths.len() - 1 {
+            println!("{}└── {}", prefix, entry.file_name().to_string_lossy());
+            if entry.file_type().expect("Invalid FileType").is_dir() {
+                recursive_walk(entry.path(), &format!("{}    ", prefix), depth + 1);
+            }
+        } else {
+            println!("{}├── {}", prefix, entry.file_name().to_string_lossy());
+            if entry.file_type().expect("Invalid FileType").is_dir() {
+                recursive_walk(entry.path(), &format!("{}│   ", prefix), depth + 1);
             }
         }
     }
